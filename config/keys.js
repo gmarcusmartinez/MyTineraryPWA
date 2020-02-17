de48defaa1dbf9e8b7f4b1f5539892f8d0e9e921
Marcus Martinez <gmarcusmartinez@gmail.com>
2020-02-17 10:30:13 -0800

    refactor

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  google: {
-    clientID:
-      '490875079714-auqtvki99440jp3fb9vtuj664pmsct2b.apps.googleusercontent.com',
-    clientSecret: 'TndRlnAzBxJ9utklCJyjDhuJ'
-  }
-}
--- server.js
+++ server.js
@@ -1,27 +1,17 @@
-const path = require('path')
-const express = require('express')
-const connectDB = require('./config/db')
+const express = require("express");
+const dotenv = require("dotenv");
 
-const app = express()
-connectDB()
+dotenv.config({ path: "./config/config.env" });
 
-app.use(express.json({ extended: false }))
+const auth = require("./routes/auth");
+const cities = require("./routes/cities");
 
-app.use('/users', require('./routes/users'))
-app.use('/cities', require('./routes/cities'))
-app.use('/reviews', require('./routes/reviews'))
-app.use('/activities', require('./routes/activities'))
-app.use('/itineraries', require('./routes/itineraries'))
+const app = express();
+app.use(express.json());
 
-// Serve Static Assets in Production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'))
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-  })
-}
+// Mount Routers
+app.use("/api/auth", auth);
+app.use("/api/cities", cities);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port:${PORT}`));
